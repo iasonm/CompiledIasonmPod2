d45d21723fa4b2f9decca4dbbe4058a80318ca70
Iason Michailidis <iasonm57@gmail.com>
2018-09-03 14:33:35 +0300

    Initial commit

---
+++ IasonmCocoapod2.framework/Headers/IasonmCocoapod2.h
@@ -0,0 +1,19 @@
+//
+//  IasonmCocoapod2.h
+//  IasonmCocoapod2
+//
+//  Created by Iason Michailidis on 03/09/2018.
+//  Copyright © 2018 Iason Michailidis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IasonmCocoapod2.
+FOUNDATION_EXPORT double IasonmCocoapod2VersionNumber;
+
+//! Project version string for IasonmCocoapod2.
+FOUNDATION_EXPORT const unsigned char IasonmCocoapod2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IasonmCocoapod2/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,13 @@
+### Install by adding a podfile to your project with:
+```
+pod 'IasonmCocoapod', '1.2.0'
+```
+### Import framework in your class. 
+```
+import IasonmCocoapod
+```
+
+### Use:
+```
+IasonmCocoapod.logToConsole(msg: "Message to log")
+```
